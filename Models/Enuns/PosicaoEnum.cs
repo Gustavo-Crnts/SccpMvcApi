7e56dcab49fcf9f0c1872d11aa91a880252a5a14
Gustavo-Crnts <gustavo.he.fe.santos@gmail.com>
2024-12-04 22:30:44 -0300

    Início do Front do projeto

---
+++ Models/Enuns/PosicaoEnum.cs
@@ -0,0 +1,10 @@
+namespace SccpMvcApi.Models.Enuns
+{
+    public enum PosicaoEnum
+    {
+        Goleiro = 1,
+        Defensor = 2,
+        Meia = 3, 
+        Atacante = 4
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SccpMvcApi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
